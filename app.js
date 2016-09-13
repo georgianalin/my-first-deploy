4fcc8b7110bd3d269802c411073aa8f04487ed05
georgianalin <alxgeoaln@gmail.com>
2016-09-13 18:50:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
